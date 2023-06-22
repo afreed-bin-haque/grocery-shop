a165d61fbc647f0c23998a4f6afec8f5500b4ef2
Afreed Bin Haque <69814471+afreed-bin-haque@users.noreply.github.com>
2023-06-22 05:10:35 +0000

    error solved

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -32,15 +32,15 @@ class Decorate extends Document {
           <Main />
           <NextScript />
           {/* <div id="preloader"></div> */}
-          <Script src="/assets/vendor/apexcharts/apexcharts.min.js" />
-          <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" />
-          <Script src="/assets/vendor/chart.js/chart.umd.js" />
-          <Script src="/assets/vendor/echarts/echarts.min.js" />
-          <Script src="/assets/vendor/quill/quill.min.js" />
-          <Script src="/assets/vendor/simple-datatables/simple-datatables.js" />
-          <Script src="/assets/vendor/tinymce/tinymce.min.js" />
-          <Script src="/assets/vendor/php-email-form/validate.js" />
-          <Script src="/assets/js/main.js" />
+          <script defer src="/assets/vendor/apexcharts/apexcharts.min.js"></script>
+          <script defer src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+          <script defer src="/assets/vendor/chart.js/chart.umd.js"></script>
+          <script defer src="/assets/vendor/echarts/echarts.min.js"></script>
+          <script defer src="/assets/vendor/quill/quill.min.js"></script>
+          <script defer src="/assets/vendor/simple-datatables/simple-datatables.js"></script>
+          <script defer src="/assets/vendor/tinymce/tinymce.min.js"></script>
+          <script defer src="/assets/vendor/php-email-form/validate.js"></script>
+          <script defer src="/assets/js/main.js"></script>
         </body>
       </Html>
     );
