53d6398aaa836b01c51efe70b3c682fbc07ae0bb
Afreed Bin Haque <69814471+afreed-bin-haque@users.noreply.github.com>
2023-06-22 05:08:30 +0000

    error solved

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -32,15 +32,15 @@ class Decorate extends Document {
           <Main />
           <NextScript />
           {/* <div id="preloader"></div> */}
-          <script src="/assets/vendor/apexcharts/apexcharts.min.js"></script>
-          <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-          <script src="/assets/vendor/chart.js/chart.umd.js"></script>
-          <script src="/assets/vendor/echarts/echarts.min.js"></script>
-          <script src="/assets/vendor/quill/quill.min.js"></script>
-          <script src="/assets/vendor/simple-datatables/simple-datatables.js"></script>
-          <script src="/assets/vendor/tinymce/tinymce.min.js"></script>
-          <script src="/assets/vendor/php-email-form/validate.js"></script>
-          <script src="/assets/js/main.js"></script>
+          <Script src="/assets/vendor/apexcharts/apexcharts.min.js" />
+          <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" />
+          <Script src="/assets/vendor/chart.js/chart.umd.js" />
+          <Script src="/assets/vendor/echarts/echarts.min.js" />
+          <Script src="/assets/vendor/quill/quill.min.js" />
+          <Script src="/assets/vendor/simple-datatables/simple-datatables.js" />
+          <Script src="/assets/vendor/tinymce/tinymce.min.js" />
+          <Script src="/assets/vendor/php-email-form/validate.js" />
+          <Script src="/assets/js/main.js" />
         </body>
       </Html>
     );
